c172e2fe8301647dd41eb14c66adb003173c460f
Ofir Aharoni(aharonox) <aharonox@peptibase9.math.biu.ac.il>
2025-07-22 15:42:26 +0300

    shila update

--- app/app.py
+++ app/app.py
@@ -29,6 +29,6 @@ def home():
 app.register_blueprint(site_service.bp)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='127.0.0.1', port=5000)
 
 
